42bf85777d024e5cd3c4fb7a7332d4a093b0d650
Volkov Alexey <lexav2016@mail.ru>
2023-10-01 20:53:59 +0300

    Some updates

--- lab1/src/parent.c
+++ lab1/src/parent.c
@@ -34,7 +34,7 @@ int main() {
     my_pipe(pipe_fd1); my_pipe(pipe_fd2); my_pipe(pipe_fd3);
     pid_t cp1, cp2;
     if ((cp1 = create_process()) == 0) { //child1
-        if (close(pipe_fd1[1]) == 1 || close(pipe_fd2[0]) == 1) {return -1;}
+        if (close(pipe_fd1[1]) == -1 || close(pipe_fd2[0]) == -1) {return -1;}
         my_dup(pipe_fd1[0], STDIN_FILENO); my_dup(pipe_fd2[1], STDOUT_FILENO);
         execl("../build/child1", "../build/child1", NULL);
         if (close(pipe_fd1[0]) == -1 || close(pipe_fd2[1]) == -1) {return -1;}
