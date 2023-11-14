885fae78fc28c84beaceb047e822545e89dd3366
Volkov Alexey <lexav2016@mail.ru>
2023-11-15 00:27:50 +0300

    Revert "lab03: try with semaphore"
    This reverts commit b7f8ac00a7bd71df25bac455d88e83009a90c3bf.

--- lab3/src/child1.c
+++ lab3/src/child1.c
@@ -4,7 +4,6 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
-#include <semaphore.h>
 
 #define MEM_SIZE 4096
 
@@ -16,30 +15,28 @@ char tolowerc(char c) {
 }
 
 int main(int argc, char *argv[]) {
-    int fd, fd_count, fd_sem;
-    char *file_in_memory; int *count_in_memory; sem_t *semaphore_in_memory;
-    if (((fd = open(argv[0], O_RDWR, 0666)) == -1) || ((fd_count = open(argv[1], O_RDWR, 0666)) == -1) || ((fd_sem = open(argv[2], O_RDWR, 0666)) == -1)) {
+    int fd, fd_count;
+    char *file_in_memory; int *count_in_memory;
+    if (((fd = open(argv[0], O_RDWR, 0666)) == -1) || ((fd_count = open(argv[1], O_RDWR, 0666)) == -1)) {
         perror("open");
         exit(-1);
     }
-    if ((ftruncate(fd, MEM_SIZE) == -1) || (ftruncate(fd_count, MEM_SIZE) == -1) || (ftruncate(fd_sem, MEM_SIZE) == -1)) {
+    if ((ftruncate(fd, MEM_SIZE) == -1) || (ftruncate(fd_count, MEM_SIZE) == -1)) {
         perror("ftruncate");
         exit(-1);
     }
-    if (((file_in_memory = mmap(NULL, MEM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0)) == MAP_FAILED) || ((count_in_memory = mmap(NULL, MEM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd_count, 0)) == MAP_FAILED) || ((semaphore_in_memory = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED, fd_sem, 0)) == MAP_FAILED)) {
+    if (((file_in_memory = mmap(NULL, MEM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0)) == MAP_FAILED) || ((count_in_memory = mmap(NULL, MEM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd_count, 0)) == MAP_FAILED)) {
         perror("mmap");
         exit(-1);
     }
-    if ((close(fd) == -1) || (close(fd_count) == -1) || (close(fd_sem) == -1)) {
+    if ((close(fd) == -1) || (close(fd_count) == -1)) {
         perror("close");
         exit(-1);
     }
-    sem_wait(semaphore_in_memory);
     for (int i = 0; i < count_in_memory[0]; i++) {
         file_in_memory[i] = tolowerc(file_in_memory[i]);
     }
-    sem_post(semaphore_in_memory);
-    if ((munmap(count_in_memory, MEM_SIZE) == -1) || (munmap(file_in_memory, MEM_SIZE) == -1) || (munmap(semaphore_in_memory, sizeof(sem_t)) == -1)) {
+    if ((munmap(count_in_memory, MEM_SIZE) == -1) || (munmap(file_in_memory, MEM_SIZE) == -1)) {
         perror("munmap");
         exit(-1);
     }
--- lab3/src/parent.c
+++ lab3/src/parent.c
@@ -9,6 +9,8 @@
 
 #define MEM_SIZE 4096
 
+sem_t semaphore;
+
 pid_t create_process() {
     pid_t pid = fork();
     if (-1 == pid) {
@@ -19,6 +21,7 @@ pid_t create_process() {
 }
 
 int main() {
+    sem_init(&semaphore, 0, 1);
     int fd = open("file", O_RDWR | O_CREAT, 0666);
     if (fd == -1) {
         perror("open");
@@ -29,14 +32,8 @@ int main() {
         perror("open");
         return -1;
     }
-    int fd_sem = open("semaphore", O_RDWR | O_CREAT, 0666);
-    if (fd_count == -1) {
-        perror("open");
-        return -1;
-    }
     ftruncate(fd, MEM_SIZE);
     ftruncate(fd_count, MEM_SIZE);
-    ftruncate(fd_sem, sizeof(sem_t));
     char *file_in_memory = mmap(NULL, MEM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
     if (file_in_memory == MAP_FAILED) {
         perror("mmap");
@@ -47,31 +44,29 @@ int main() {
         perror("mmap");
         return -1;
     }
-    sem_t *semaphore_in_memory = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED, fd_sem, 0);
-    if (semaphore_in_memory == MAP_FAILED) {
-        perror("mmap");
-        return -1;
-    }
-    char *argv[] = {"file", "count", "semaphore", (char *) NULL};
+    char *argv[] = {"file", "count", (char *) NULL};
     my_string *p_mstr = create_string();
     printf("Enter your string: "); read_string(p_mstr);
     for (int i = 0; i < p_mstr->length; i++) {
         file_in_memory[i] = p_mstr->str[i];
     }
     count_in_memory[0] = p_mstr->length;
-    sem_init(semaphore_in_memory, 0, 1);
-    close(fd); close(fd_count); close(fd_sem);
+    close(fd); close(fd_count);
     pid_t cp1, cp2;
     if ((cp1 = create_process()) == 0) { //child1
+        sem_wait(&semaphore);
         if (execv("../build/child1", argv) == -1) {
             perror("execv");
             return -1;
         }
+        sem_post(&semaphore);
     } else if (cp1 > 0 && (cp2 = create_process()) == 0) { //child2
+        sem_wait(&semaphore);
         if (execv("../build/child2", argv) == -1) {
             perror("execv");
             return -1;
         }
+        sem_post(&semaphore);
     } else { //parent
         wait(NULL);
         printf("Result: "); 
@@ -81,7 +76,7 @@ int main() {
         printf("\n");
         munmap(count_in_memory, MEM_SIZE);
         munmap(file_in_memory, MEM_SIZE);
-        munmap(semaphore_in_memory, sizeof(sem_t));
+        sem_destroy(&semaphore);
     }
     return 0;
 }
